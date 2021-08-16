9d7f97aa88592e58c8ef2b557f3dc39592aeb84a
tungdt <tungdt1412@gmail.com>
2021-08-16 14:41:06 +0700

    Add Entity

---
+++ ShopSolution.Data/Entities/AppRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopSolution.Data.Entities
+{
+    public class AppRole : IdentityRole<Guid>
+    {
+        public string Description { get; set; }
+    }
+}
