9210945ee1483418351f77af1975f9604f19aa4c
tungdt <tungdt1412@gmail.com>
2021-08-24 10:11:20 +0700

    Create User

--- ShopSolution.AdminApp/Controllers/UserController.cs
+++ ShopSolution.AdminApp/Controllers/UserController.cs
@@ -17,7 +17,7 @@ using System.Threading.Tasks;
 
 namespace ShopSolution.AdminApp.Controllers
 {
-    public class UserController : Controller
+    public class UserController : BaseController
     {
         private readonly IUserApiClient _userApiClient;
         private readonly IConfiguration _configuration;
@@ -29,6 +29,7 @@ namespace ShopSolution.AdminApp.Controllers
         public async Task<IActionResult> Index(string keyword, int pageIndex = 1, int PageSize = 10)
         {
             var sessions = HttpContext.Session.GetString("Token");
+
             var request = new GetUserPagingRequest()
             {
                 BearerToken = sessions,
@@ -41,41 +42,23 @@ namespace ShopSolution.AdminApp.Controllers
 
             return View(data);
         }
-
         [HttpGet]
-        public async Task<IActionResult> Login()
+        public IActionResult Create()
         {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginRequest request)
+        public async Task<IActionResult> Create(RegisterRequest request)
         {
             if (!ModelState.IsValid)
-            {
-                return View(ModelState);
-            }
-            var token = await _userApiClient.Authenticate(request);
-
-            var userPrincipal = this.ValidateToken(token);
-
-            var authProperties = new AuthenticationProperties
-            {
-                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                IsPersistent = false
-            };
-
-            HttpContext.Session.SetString("Token", token);
-
-            await HttpContext.SignInAsync(
-                       CookieAuthenticationDefaults.AuthenticationScheme,
-                       userPrincipal,
-                       authProperties);
-
-            return RedirectToAction("Index", "Home");
+                return View();
+            var result =  await _userApiClient.RegisterUser(request);
+            if (result)
+                return RedirectToAction("Index");
+            return View(request);
         }
-
+        
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
@@ -83,22 +66,6 @@ namespace ShopSolution.AdminApp.Controllers
             HttpContext.Session.Remove("Token");
             return RedirectToAction("Login", "User");
         }
-        private ClaimsPrincipal ValidateToken(string jwtToken)
-        {
-            IdentityModelEventSource.ShowPII = true;
-
-            SecurityToken validatedToken;
-            TokenValidationParameters validationParameters = new TokenValidationParameters();
-
-            validationParameters.ValidateLifetime = true;
-
-            validationParameters.ValidAudience = _configuration["Tokens:Issuer"];
-            validationParameters.ValidIssuer = _configuration["Tokens:Issuer"];
-            validationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
-
-            ClaimsPrincipal principal = new JwtSecurityTokenHandler().ValidateToken(jwtToken, validationParameters, out validatedToken);
-
-            return principal;
-        }
+        
     }
 }
